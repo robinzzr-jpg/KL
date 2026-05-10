a8fa1d5915c136d1b674fe278339481729b1486e
robinzzr-jpg <robinzzr@gmail.com>
2026-05-10 12:49:37 +0800

    Add src/index.js React entry point

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import KLTravelGuide from './kl-family-travel-guide';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <KLTravelGuide />
+  </React.StrictMode>
+);
